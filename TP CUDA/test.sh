858a4dcdf5bf92e256bcac2b534b570b967c1f32
tianjianye <44018955+tianjianye@users.noreply.github.com>
2020-01-23 18:55:29 +0100

    cuda

--- TP CUDA/test.sh
+++
@@ -1,2 +0,0 @@
-/usr/local/cuda/bin/nvcc cuda_td_1.cu
-./a.out dataset/vector/input0.raw dataset/vector/input1.raw dataset/vector/output.raw
---
+++ TP CUDA/test1.sh
@@ -0,0 +1,2 @@
+/usr/local/cuda/bin/nvcc cuda_td_1.cu
+./a.out dataset/vector/input0.raw dataset/vector/input1.raw dataset/vector/output.raw
