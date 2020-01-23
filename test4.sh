41e194d07a4d6fe0503ea4de9a71b4d8a15ee220
Ye Tianjian <tye@if501-208-13.insa-lyon.fr>
2020-01-23 19:16:55 +0100

    commit2

---
+++ test4.sh
@@ -0,0 +1,2 @@
+/usr/local/cuda/bin/nvcc cuda_td_4.cu
+./a.out dataset/matrix/input0.raw dataset/matrix/input1.raw dataset/matrix/output.raw
---
+++ test5.sh
@@ -0,0 +1,2 @@
+/usr/local/cuda/bin/nvcc cuda_pattern_td_1.cu
+./a.out dataset_td_pattern_1/0input.raw dataset_td_pattern_1/0output.raw
